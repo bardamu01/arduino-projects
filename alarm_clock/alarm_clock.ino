69a8bbc9820c13e78b9db76a8f7ee0965e62be12
bardamu01 <bardamu01@github.com>
2014-12-22 14:38:41 +0200

    moving to diplay function

--- alarm_clock/alarm_clock.ino
+++ alarm_clock/alarm_clock.ino
@@ -2,14 +2,31 @@
 #include <TimeAlarms.h> 
 
 #define ALARM_PIN 8
+#define SNOOZE_PIN 7
+#define CANCEL_PIN 6
 
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
 #define TIME_HEADER  'T'   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 #include <LiquidCrystal.h>
+#define NR_CHARS_PER_ROW 16
+#define ROWS 2
 //LiquidCrystal(rs, enable, d4, d5, d6, d7) 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void display(char* msg){
+	lcd.clear();
+	String s = String(msg);
+	int newLine = s.indexOf("\n");
+	if ( newLine == -1 ){
+		lcd.print(msg);
+	} else {
+		lcd.print(s.substring(0, newLine));
+		lcd.setCursor(0, 1);
+		lcd.print(s.substring(newLine+1, s.length()));
+	}
+}
 
 void processSyncMessage(){
 	// if time sync available from the serial port
@@ -44,24 +61,39 @@ String getTime(){
 	return String(String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); 
 }
 
+void beep(int freq, int period, int silence){
+	analogWrite(ALARM_PIN, freq); 
+	delay(period);
+	analogWrite(ALARM_PIN, 0);
+	delay(silence);
+}
+
 void soundAlarm(){
-	analogWrite(ALARM_PIN, 255); 
-	delay(500);
-	analogWrite(ALARM_PIN, 0); 
+	beep(200, 250, 250);
+	beep(200, 250, 250);
+	beep(255, 250, 0);
+}
+
+void alarm(){
+	lcd.clear();
+	display("Beep! Beep!\nSNOOZE or CANCEL");
+	soundAlarm();
+	if (digitalRead(SNOOZE_PIN) == HIGH){
+		soundAlarm();
+	}
 }
 
 void setup(){
 	Serial.begin(9600);
-	lcd.begin(16, 2);
+	lcd.begin(NR_CHARS_PER_ROW, ROWS);
 	pinMode(ALARM_PIN, OUTPUT);
+	pinMode(SNOOZE_PIN, INPUT_PULLUP);
 	
-	lcd.print("Waiting on time");
-	lcd.setCursor(0,1);
-	lcd.print("sync from serial");
+	display("Waiting on time\nsync from serial");
 	syncTime();
 	
 	time_t t = now();
-	Alarm.alarmOnce(hour(t), minute(t), second(t) + 10, soundAlarm);
+	Alarm.alarmOnce(hour(t), minute(t), second(t) + 10, alarm);
 }
 
 void loop(){
